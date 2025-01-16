1602712a053c78b554ae960bf6947b90dc97a31d
Demahrix <hamed.zgr@gmail.com>
2025-01-16 17:08:34 +0000

    Update

--- lib/auth_model.dart
+++ lib/auth_model.dart
@@ -2,13 +2,19 @@
 class AuthModel<R> {
 
   final R reference;
-  final String accessTokens;
+  final String accessToken;
   final String refreshToken;
 
   AuthModel({
     required this.reference,
-    required this.accessTokens,
+    required this.accessToken,
     required this.refreshToken
   });
+
+  Map<String, dynamic> toMap() => {
+    'reference': reference,
+    'accessToken': accessToken,
+    'refreshToken': refreshToken
+  };
 
 }
